ec412e2fa2ab29d6fc1907e9f1f4ee2a2a142000
Marquilly <theomarquilly19@gmail.com>
2025-12-02 08:40:53 +0100

    coche

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,10 @@
 <!--
-    Les photos appartiennent à un album. 
-    Les photos ont également des étiquettes
+    Les photos appartiennent à un album. => fonctionne
+    Les photos ont également des étiquettes => fonctionne
     Il est possible de voir l'ensemble des albums => fonctionne
-    Il est possible de voir les photos d'un album
-    Lorsqu'on clique sur une photo, on la voit en grand (un peu de JS, ca ne fait pas de mal).
-    Il doit être possible de filtrer les photos visualisées par rapport aux étiquettes ou à une recherche sur le titre
+    Il est possible de voir les photos d'un album => fonctionne
+    Lorsqu'on clique sur une photo, on la voit en grand (un peu de JS, ca ne fait pas de mal). => fonctionne
+    Il doit être possible de filtrer les photos visualisées par rapport aux étiquettes ou à une recherche sur le titre => fonctionne
     Il doit être possible d'insérer une photo dans un album (d'abord via une url, ensuite via l'upload de photos) => fonctionne
     Il doit être possible de supprimer une photo d'un album (et donc la photo)
     Il doit être possible de trier l'affichage suivant les notes, les titres
